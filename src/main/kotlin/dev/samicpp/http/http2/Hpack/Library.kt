3eef29dde5a0cb0fc79121eba63344f7832e14f3
samic++ <samichentouf+git2@gmail.com>
2025-09-19 11:17:59 +0200

    add static and dynamic table

--- src/main/kotlin/dev/samicpp/http/http2/Hpack/Library.kt
+++ src/main/kotlin/dev/samicpp/http/http2/Hpack/Library.kt
@@ -1,65 +1,97 @@
 package dev.samicpp.http.hpack
+// https://datatracker.ietf.org/doc/html/rfc7541
 
-val StaticTable=listOf(
-    ":authority" to "",
-    ":method" to "GET",
-    ":method" to "POST",
-    ":path" to "/",
-    ":path" to "/index.html",
-    ":scheme" to "http",
-    ":scheme" to "https",
-    ":status" to "200",
-    ":status" to "204",
-    ":status" to "206",
-    ":status" to "304",
-    ":status" to "400",
-    ":status" to "404",
-    ":status" to "500",
-    "accept-" to "",
-    "accept-encoding" to "gzip to deflate",
-    "accept-language" to "",
-    "accept-ranges" to "",
-    "accept" to "",
-    "access-control-allow-origin" to "",
-    "age" to "",
-    "allow" to "",
-    "authorization" to "",
-    "cache-control" to "",
-    "content-disposition" to "",
-    "content-encoding" to "",
-    "content-language" to "",
-    "content-length" to "",
-    "content-location" to "",
-    "content-range" to "",
-    "content-type" to "",
-    "cookie" to "",
-    "date" to "",
-    "etag" to "",
-    "expect" to "",
-    "expires" to "",
-    "from" to "",
-    "host" to "",
-    "if-match" to "",
-    "if-modified-since" to "",
-    "if-none-match" to "",
-    "if-range" to "",
-    "if-unmodified-since" to "",
-    "last-modified" to "",
-    "link" to "",
-    "location" to "",
-    "max-forwards" to "",
-    "proxy-authenticate" to "",
-    "proxy-authorization" to "",
-    "range" to "",
-    "referer" to "",
-    "refresh" to "",
-    "retry-after" to "",
-    "server" to "",
-    "set-cookie" to "",
-    "strict-transport-security" to "",
-    "transfer-encoding" to "",
-    "user-agent" to "",
-    "vary" to "",
-    "via" to "",
-    "www-authenticate" to "",
+data class Header(
+    val name:String,
+    val value:String?=null
 )
+
+// Appendix A #appendix-A
+class StaticTable{
+    // needs to be offset by +1 before use
+    val entries=listOf( // 61 headers
+        Header(":authority"),
+        Header(":method", "GET"),
+        Header(":method", "POST"),
+        Header(":path", "/"),
+        Header(":path", "/index.html"),
+        Header(":scheme", "http"),
+        Header(":scheme", "https"),
+        Header(":status", "200"),
+        Header(":status", "204"),
+        Header(":status", "206"),
+        Header(":status", "304"),
+        Header(":status", "400"),
+        Header(":status", "404"),
+        Header(":status", "500"),
+        Header("accept-"),
+        Header("accept-encoding", "gzip to deflate"),
+        Header("accept-language"),
+        Header("accept-ranges"),
+        Header("accept"),
+        Header("access-control-allow-origin"),
+        Header("age"),
+        Header("allow"),
+        Header("authorization"),
+        Header("cache-control"),
+        Header("content-disposition"),
+        Header("content-encoding"),
+        Header("content-language"),
+        Header("content-length"),
+        Header("content-location"),
+        Header("content-range"),
+        Header("content-type"),
+        Header("cookie"),
+        Header("date"),
+        Header("etag"),
+        Header("expect"),
+        Header("expires"),
+        Header("from"),
+        Header("host"),
+        Header("if-match"),
+        Header("if-modified-since"),
+        Header("if-none-match"),
+        Header("if-range"),
+        Header("if-unmodified-since"),
+        Header("last-modified"),
+        Header("link"),
+        Header("location"),
+        Header("max-forwards"),
+        Header("proxy-authenticate"),
+        Header("proxy-authorization"),
+        Header("range"),
+        Header("referer"),
+        Header("refresh"),
+        Header("retry-after"),
+        Header("server"),
+        Header("set-cookie"),
+        Header("strict-transport-security"),
+        Header("transfer-encoding"),
+        Header("user-agent"),
+        Header("vary"),
+        Header("via"),
+        Header("www-authenticate"),
+    )
+}
+
+// 4 #autoid-16
+class DynamicTable(var maxSize:Int=4096){
+    private val entries=ArrayDeque<Header>(0)
+    private var curSize:Int=0
+
+    fun add(field: Header){
+        entries.addFirst(field)
+        curSize+=field.name.length+(field.value?.length?:0)+32
+        evict()
+    }
+    fun get(index:Int):Header{
+        return entries[index]
+    }
+
+    private fun evict(){
+        while(entries.isNotEmpty()&&curSize>maxSize){
+            val field=entries.removeLast()
+            curSize-=field.name.length+(field.value?.length?:0)+32
+        }
+    }
+}
